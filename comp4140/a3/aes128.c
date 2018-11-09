6cac26da2e978b09becc42d99edc091e16bd6c58
ColinDyson <colin8483@gmail.com>
2018-11-08 19:33:03 -0600

    wip

--- comp4140/a3/aes128.c
+++ comp4140/a3/aes128.c
@@ -9,7 +9,7 @@ int read_input(const char *inFile, int file_length, byte *input_string) {
   FILE *in = fopen(inFile, "r");
 
   if (in == NULL) {
-    printf("ERROR opening %s\n", inFile);
+    printf("ERROR: opening %s\n", inFile);
     return(1);
   }
   
@@ -21,23 +21,26 @@ int read_input(const char *inFile, int file_length, byte *input_string) {
   return 0;
 }
 
-int print_bytes_as_hex(int string_length, byte *string ) {
-  if (string == NULL) {
-    printf("ADADADQDASDF");
+/*Print the contents of string as bytes represented by two hexadecimal characters.*/
+int print_bytes_as_hex(int string_length, byte *byte_string ) {
+  if (byte_string == NULL) {
+    printf("ERROR: attempting to print string with value NULL\n");
+    return 1;
   }
   for (int i = 0; i < string_length; i++) {
-    printf("%02x ", string[i]);
+    printf("%02x ", byte_string[i]);
   }
 
   return 0;
 }
 
+/*Counts the number of bytes of message text in the input. Excludes whitespace.*/
 int find_input_length(const char *file_name) {
   int file_length = 0;
   FILE *in = fopen(file_name, "r");
 
   if (in == NULL) {
-    printf("ERROR opening %s\n", file_name);
+    printf("ERROR: opening %s\n", file_name);
     return(1);
   }
 
