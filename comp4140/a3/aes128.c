ecfde84818d1e488daa3daa4b98436354e3f4cd0
ColinDyson <colin8483@gmail.com>
2018-11-20 20:02:40 -0600

    wip

--- comp4140/a3/aes128.c
+++ comp4140/a3/aes128.c
@@ -6,6 +6,15 @@
 typedef uint8_t byte;
 typedef uint32_t word;
 
+const int N_k = 4; //length of key, in 32-bit words
+const int N_b = 4; //number of columns in the State, in 32-bit words
+const int N_r = 10; //number of rounds
+const int N_s = 256; //size of s-box (by definition), in 8-bit bytes
+const char *const SBOX_NAME = "aes_sbox.txt";
+const char *const INV_SBOX_NAME = "aes_inv_sbox.txt";
+byte *sbox;
+byte *inv_sbox;
+
 int read_input(const char *inFile, int file_length, byte *input_string) {
   FILE *in = fopen(inFile, "r");
 
@@ -24,7 +33,8 @@ int read_input(const char *inFile, int file_length, byte *input_string) {
 
 /*Print the contents of string as bytes represented by two hexadecimal characters.*/
 int print_bytes_as_hex(int string_length, byte *byte_string ) {
-  const int WORDS_PER_LINE = 4; //number of 32-bit words to print before adding a newline    
+  const int WORDS_PER_LINE = 4; //number of 32-bit words to print before adding a newline
+
   if (byte_string == NULL) {
     printf("ERROR: Attempting to print string with value NULL\n");
     return 1;
@@ -67,17 +77,58 @@ int bytes_to_words(int n) {
   return n / sizeof(word) / sizeof(byte);
 }
 
+//Substitutes each byte in the word with the corresponding byte in the S-box
+word* sub_word(word *w) {
+  byte first_nibble;
+  byte second_nibble;
+  byte *curr_byte;
+
+  for (int i = 0; i < words_to_bytes(1); i++) {
+    curr_byte = ((byte *)&w + i);
+    first_nibble = (*curr_byte >> 4);
+    second_nibble = (*curr_byte & 0x0F);
+
+    *curr_byte = sbox[16 * first_nibble + second_nibble];
+  }
+
+  return w;
+}
+
 int cipher(byte *in, byte *out) {
   print_bytes_as_hex(16, in);
   return 0;
 }
 
-const int N_k = 4; //length of key, in 32-bit words
-const int N_b = 4; //number of columns in the State, in 32-bit words
-const int N_r = 10; //number of rounds
-const int N_s = 256; //size of s-box (by definition), in 8-bit bytes
-const char *const SBOX_NAME = "aes_sbox.txt";
-const char *const INV_SBOX_NAME = "aes_inv_sbox.txt";
+int key_expansion(byte *key, word *key_schedule) {
+  word *temp;
+
+  int i = 0;
+
+  while (i < N_k) {
+    *((byte*)(&key_schedule) + 3) = key[4 * i + 3];
+    *((byte*)(&key_schedule) + 2) = key[4 * i + 2];
+    *((byte*)(&key_schedule) + 1) = key[4 * i + 1];
+    *((byte*)(&key_schedule) + 0) = key[4 * i + 0];
+    //w[i] = word(key[4*i], key[4*i+1], key[4*i+2], key[4*i+3])
+    i++;
+  }
+
+  i = N_k;
+
+  while (i < N_b * (N_r + 1)) {
+    printf("testing\n");
+    temp = &key_schedule[i - 1];
+    printf("again\n");
+    if (i % N_k == 0) {
+      //temp = sub_word(rot_word(temp)) & r_con[i / N_k];
+      printf("%x\n", temp);
+      temp = sub_word(temp);
+    }
+    key_schedule[i] = key_schedule[i - N_k] & *temp;
+    i++;
+  }
+  return 0;
+}
 
 int main(int argc, char const *argv[]) {
   const char *output_filename = "output.txt";
@@ -97,22 +148,24 @@ int main(int argc, char const *argv[]) {
   byte *input_string = calloc(sizeof(byte), input_length);
   byte *output_string = calloc(sizeof(byte), input_length);
   byte *key = calloc(sizeof(word), N_k);
-  byte *sbox = calloc(sizeof(byte), N_s);
-  byte *inv_sbox = calloc(sizeof(byte), N_s);
+  word *key_schedule = calloc(sizeof(word), N_b * (N_r + 1));
+  sbox = calloc(sizeof(byte), N_s);
+  inv_sbox = calloc(sizeof(byte), N_s);
 
   read_input(argv[1], input_length, input_string);
   read_input(argv[2], words_to_bytes(N_k), key);
   read_input(SBOX_NAME, N_s, sbox);
   read_input(INV_SBOX_NAME, N_s, inv_sbox);
 
-  int n_blocks = input_length / 4 / N_b; //number of blocks = length(in bytes) / 4 bytes per row / num rows
+  key_expansion(key, key_schedule);
+  // int n_blocks = input_length / 4 / N_b; //number of blocks = length(in bytes) / 4 bytes per row / num rows
 
-  for (int i = 0; i < n_blocks; i++) {
-    byte *curr_input_block = &input_string[i * 4 * N_b];
-    byte *curr_output_block = &output_string[i * 4 * N_b]; //location in byte array = block number * 4 bytes per row * num rows
+  // for (int i = 0; i < n_blocks; i++) {
+  //   byte *curr_input_block = &input_string[i * 4 * N_b];
+  //   byte *curr_output_block = &output_string[i * 4 * N_b]; //location in byte array = block number * 4 bytes per row * num rows
 
-    cipher(curr_input_block, curr_output_block);
-  }
+  //   cipher(curr_input_block, curr_output_block);
+  // }
 
   print_bytes_as_hex(input_length, input_string);
   print_bytes_as_hex(N_k * 4, key);
